0def19c5939afdf7d8475a925632bca3beec0c50
amitquantavid <amit.kumar@quantavid.com>
2021-02-16 10:45:22 +0530

    changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,18 +3,18 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
         <>
-            <div className="container-fluid nav bg">
+            <div className="container-fluid nav_bg">
                 <div className="row">
                     <div className="col-12 max-auto">
-                        <nav className="navbar  navbar-expand-lg navbar-light bg-light">
+                        <nav className="navbar navbar-expand-lg navbar-light bg-light">
                             <div className="container-fluid">
                                 <NavLink className="navbar-brand menu-active ml-5" to="/">Navbar</NavLink>
                                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
 
-                                <div className="collapse navbar-collapse mr-5" id="navbarSupportedContent">
-                                    <ul className="navbar-nav ml-auto">
+                                <div className="collapse navbar-collapse  flex-grow-0" id="navbarSupportedContent">
+                                    <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                                         <li className="nav-item active">
                                             <NavLink className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
                                         </li>
